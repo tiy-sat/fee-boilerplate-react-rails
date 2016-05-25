f0ddf95f13391c3494782ee8c23dcf4bf6f7218c
Jason Vanderslice <jlvanderslice@gmail.com>
2016-05-25 14:11:08 -0500

    updated heading text for specific message

--- www/app/modules/App.js
+++ www/app/modules/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default React.createClass({
   render() {
     return (
-      <h1 className="heading">Hello!</h1>
+      <h1 className="heading">Welcome to FEE Boilerplate Rails!</h1>
     )
   }
 })
